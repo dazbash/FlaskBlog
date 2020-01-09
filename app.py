2ff604c25bfbea028c487bddaeb1259404e745d3
dazbash <javadolaemeh49@gmail.com>
2020-01-09 10:53:17 +0330

    09 T

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Development
 from flask_migrate import Migrate
 
-
-
 app = Flask(__name__)
 app.config.from_object(Development)
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from views import index
+
 from mod_admin import admin
 from  mod_users import users
+from mod_blog import blog
 
 
 app.register_blueprint(admin)
 app.register_blueprint(users)
+app.register_blueprint(blog)
--- mod_blog/__init__.py
+++ mod_blog/__init__.py
@@ -2,3 +2,5 @@ from flask import Blueprint
 
 
 blog = Blueprint('blog', __name__, url_prefix='/blog/')
+
+from . import models
